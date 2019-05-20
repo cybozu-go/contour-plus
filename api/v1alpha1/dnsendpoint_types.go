cbe6ea68f497cc56d6deffae33df116659b273cd
dulltz <isrgnoe@gmail.com>
2019-05-20 15:22:12 +0900

    Add specification for DNSEndpoint

--- api/v1alpha1/dnsendpoint_types.go
+++ api/v1alpha1/dnsendpoint_types.go
@@ -16,22 +16,132 @@ limitations under the License.
 package v1alpha1
 
 import (
+	"sort"
+	"strings"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
+// Labels store metadata related to the endpoint
+// it is then stored in a persistent storage via serialization
+type Labels map[string]string
+
+// TTL is a structure defining the TTL of a DNS record
+type TTL int64
+
+// IsConfigured returns true if TTL is configured, false otherwise
+func (ttl TTL) IsConfigured() bool {
+	return ttl > 0
+}
+
+// Targets is a representation of a list of targets for an endpoint.
+type Targets []string
+
+// NewTargets is a convenience method to create a new Targets object from a vararg of strings
+func NewTargets(target ...string) Targets {
+	t := make(Targets, 0, len(target))
+	t = append(t, target...)
+	return t
+}
+
+func (t Targets) String() string {
+	return strings.Join(t, ";")
+}
+
+func (t Targets) Len() int {
+	return len(t)
+}
+
+func (t Targets) Less(i, j int) bool {
+	return t[i] < t[j]
+}
+
+func (t Targets) Swap(i, j int) {
+	t[i], t[j] = t[j], t[i]
+}
+
+// Same compares to Targets and returns true if they are completely identical
+func (t Targets) Same(o Targets) bool {
+	if len(t) != len(o) {
+		return false
+	}
+	sort.Stable(t)
+	sort.Stable(o)
+
+	for i, e := range t {
+		if e != o[i] {
+			return false
+		}
+	}
+	return true
+}
+
+// IsLess should fulfill the requirement to compare two targets and chosse the 'lesser' one.
+// In the past target was a simple string so simple string comparison could be used. Now we define 'less'
+// as either being the shorter list of targets or where the first entry is less.
+// FIXME We really need to define under which circumstances a list Targets is considered 'less'
+// than another.
+func (t Targets) IsLess(o Targets) bool {
+	if len(t) < len(o) {
+		return true
+	}
+	if len(t) > len(o) {
+		return false
+	}
+
+	sort.Sort(t)
+	sort.Sort(o)
+
+	for i, e := range t {
+		if e != o[i] {
+			return e < o[i]
+		}
+	}
+	return false
+}
+
+// ProviderSpecificProperty holds the name and value of a configuration which is specific to individual DNS providers
+type ProviderSpecificProperty struct {
+	Name  string `json:"name,omitempty"`
+	Value string `json:"value,omitempty"`
+}
+
+// ProviderSpecific holds configuration which is specific to individual DNS providers
+type ProviderSpecific []ProviderSpecificProperty
+
+// Endpoint defines components of DNSEndpoint
+type Endpoint struct {
+	// The hostname of the DNS record
+	DNSName string `json:"dnsName,omitempty"`
+	// The targets the DNS record points to
+	Targets Targets `json:"targets,omitempty"`
+	// RecordType type of record, e.g. CNAME, A, SRV, TXT etc
+	RecordType string `json:"recordType,omitempty"`
+	// TTL for the record
+	RecordTTL TTL `json:"recordTTL,omitempty"`
+	// Labels stores labels defined for the Endpoint
+	// +optional
+	Labels Labels `json:"labels,omitempty"`
+	// ProviderSpecific stores provider specific config
+	// +optional
+	ProviderSpecific ProviderSpecific `json:"providerSpecific,omitempty"`
+}
+
 // DNSEndpointSpec defines the desired state of DNSEndpoint
 type DNSEndpointSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+	Endpoints []*Endpoint `json:"endpoints,omitempty"`
 }
 
 // DNSEndpointStatus defines the observed state of DNSEndpoint
 type DNSEndpointStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+	ObservedGeneration int64 `json:"observedGeneration,omitempty"`
 }
 
 // +kubebuilder:object:root=true
--- go.mod
+++ go.mod
@@ -4,9 +4,10 @@ go 1.12
 
 require (
 	github.com/go-logr/logr v0.1.0
-	github.com/onsi/ginkgo v1.6.0
-	github.com/onsi/gomega v1.4.2
-	golang.org/x/net v0.0.0-20180906233101-161cd47e91fd
+	github.com/kubernetes-incubator/external-dns v0.5.14 // indirect
+	github.com/onsi/ginkgo v1.8.0
+	github.com/onsi/gomega v1.5.0
+	golang.org/x/net v0.0.0-20190311183353-d8887717615a
 	k8s.io/apimachinery v0.0.0-20190404173353-6a84e37a896d
 	k8s.io/client-go v11.0.1-0.20190409021438-1a26190bd76a+incompatible
 	sigs.k8s.io/controller-runtime v0.2.0-beta.1
